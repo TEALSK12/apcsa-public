8b40ab5bf7afd45c5edcaeb86897d37c08bdcc61
Andrew Spiece <andrew@tealsk12.org>
2020-03-23 23:24:57 -0400

    Delete Replace.cmd

--- Replace.cmd
+++
@@ -1,5 +0,0 @@
-
-Set-Content -Path 'C:\file.txt' -Value 'foo bar baz'
-$content = Get-Content -Path 'C:\file.txt'
-PS> $newContent = $content -replace 'foo', 'bar'
-$newContent | Set-Content -Path 'C:\file.txt'
